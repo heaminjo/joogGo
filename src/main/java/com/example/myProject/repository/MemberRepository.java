fce44739cac6f995e81e7b9c6cddefe4fcd5c83d
heaminjo <157016835+heaminjo@users.noreply.github.com>
2024-10-07 23:33:02 +0900

    CRUD 메소드 추가

--- src/main/java/com/example/myProject/repository/MemberRepository.java
+++ src/main/java/com/example/myProject/repository/MemberRepository.java
@@ -4,4 +4,7 @@ import com.example.myProject.Entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member,Long> {
+      public Optional<Member> findByEmail(String email);
+      public Optional<Member> findByLoginIdAndLoginPw(String loginId,String loginPw);
+
 }
