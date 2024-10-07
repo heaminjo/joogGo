fce67af0f6c2093bda616d59522ff7d0e1fb4c26
heaminjo <157016835+heaminjo@users.noreply.github.com>
2024-10-07 23:30:41 +0900

    게시판 저장 테스트 추가

--- src/test/java/com/example/myProject/repository/BoardRepositoryTest.java
+++ src/test/java/com/example/myProject/repository/BoardRepositoryTest.java
@@ -32,8 +32,25 @@ public class BoardRepositoryTest {
     }
 
 
-    public void createBoardInfo(){
+    public Board createBoardInfo(){
         Board board = new Board();
+        board.setTitle("옷 판다");
+        board.setContent("치마 입니다.");
+        board.setLocation("경기도 성남시");
+        board.setReliability(70);
+        board.setPrice(26000);
+        board.setViews(100);
 
+        return board;
+    }
+
+    @Test
+    @DisplayName("게시판 저장 테스트")
+    public void saveBoardTest(){
+        Board board = createBoardInfo();
+        Board saveBoard = boardRepository.save(board);
+        System.out.println(saveBoard.getTitle());
+        System.out.println(saveBoard.getContent());
+        System.out.println(saveBoard.getLocation());
     }
 }
